a949d63f5841c3ca0b17b9fc3869fe5c446c2787
Igor <igorex2102@wp.pl>
2024-09-22 00:56:55 +0200

    Refactor HTML and gitignore files

---
+++ README.md
@@ -0,0 +1,130 @@
+<!-- Improved compatibility of back to top link: See: https://github.com/othneildrew/Best-README-Template/pull/73 -->
+
+<a name="readme-top"></a>
+
+<!--
+*** Thanks for checking out the Best-README-Template. If you have a suggestion
+*** that would make this better, please fork the repo and create a pull request
+*** or simply open an issue with the tag "enhancement".
+*** Don't forget to give the project a star!
+*** Thanks again! Now go create something AMAZING! :D
+-->
+
+<!-- PROJECT SHIELDS -->
+<!--
+*** I'm using markdown "reference style" links for readability.
+*** Reference links are enclosed in brackets [ ] instead of parentheses ( ).
+*** See the bottom of this document for the declaration of the reference variables
+*** for contributors-url, forks-url, etc. This is an optional, concise syntax you may use.
+*** https://www.markdownguide.org/basic-syntax/#reference-style-links
+-->
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://github.com/github_username/repo_name">
+    <img src="logos/weather-news.png" alt="Logo" width="80" height="80">
+  </a>
+
+<h3 align="center">Weather-Checker</h3>
+
+  <p align="center">
+    This TypeScript-based application allows users to check the weather forecast for the next three days. Users can provide their location by entering a city name or by sharing their location through the Navigator API. The app fetches real-time weather data and presents a detailed forecast, helping users stay informed about upcoming weather conditions.
+    <br />
+    <br />
+    <br />
+    <a href="https://github.com/IgrWrz21/Weather-Checker.git">View Demo</a>
+    
+    
+  </p>
+</div>
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#roadmap">Roadmap</a></li>
+    
+  </ol>
+</details>
+
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+
+<img src="logos/appScreen.PNG" alt="appScreen" width="800" height="400">
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+### Built With
+
+- ![HTML]
+- ![CSS]
+- ![JavaScript]
+- ![TypeScript]
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- GETTING STARTED -->
+
+## Getting Started
+
+This is an example of how you may give instructions on setting up your project locally.
+To get a local copy up and running follow these simple example steps.
+
+### Installation
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/IgrWrz21/To-do-List.git
+   ```
+2. Open terminal and install dependencies
+
+```sh
+   cd <project_name>
+   npm install
+```
+
+2. Build and run the project
+
+```sh
+   npm start
+```
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- USAGE EXAMPLES -->
+
+<!-- ROADMAP -->
+
+## Roadmap
+
+- [x] Add additional weather information
+- [x] Add geting location from user by navigator API
+- [ ] Add popup with information when city wasn't found
+- [ ] Add more days to display
+  - [ ] Add functionality to slides those days
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- ACKNOWLEDGMENTS -->
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+
+[CSS]: https://img.shields.io/badge/CSS3-grey?style=for-the-badge&logo=css3&logoColor=1572B6
+[HTML]: https://img.shields.io/badge/HTML5-grey?style=for-the-badge&logo=html5
+[JavaScript]: https://img.shields.io/badge/JAVASCRIPT-grey?style=for-the-badge&logo=javascript
+[TypeScript]: https://img.shields.io/badge/TypeScript-grey?style=for-the-badge&logo=typescript
---
+++ src/app.ts
@@ -0,0 +1,224 @@
+import axios from "axios";
+import dotenv from "dotenv";
+import "./app.css";
+dotenv.config();
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY!;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY!;
+
+type GoogleGeoResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
+class Input {
+  formElement: HTMLFormElement;
+  inputElement: HTMLInputElement;
+  cityNameElement: HTMLHeadingElement;
+  shrLocationButton: HTMLButtonElement;
+
+  constructor() {
+    this.formElement = document.querySelector("form")! as HTMLFormElement;
+    this.inputElement = document.getElementById("address")! as HTMLInputElement;
+    this.cityNameElement = document.getElementById(
+      "cityName"
+    )! as HTMLHeadingElement;
+    this.shrLocationButton = document.querySelector(".locationBtn")!;
+    this.configure();
+  }
+
+  configure() {
+    this.shrLocationButton.addEventListener(
+      "click",
+      this.getCurrentPostion.bind(this)
+    );
+
+    this.formElement.addEventListener(
+      "submit",
+      this.searchAddressHandler.bind(this)
+    );
+  }
+  successCallBack(pos: GeolocationPosition) {
+    const crd = pos.coords;
+    const { latitude, longitude } = crd;
+    const newCord = `${latitude},${longitude}`;
+
+    this.getWeathereData(newCord);
+  }
+  errorCallBack(err: GeolocationPositionError) {
+    alert(`ERROR(${err.code}): ${err.message}`);
+  }
+  getCurrentPostion() {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+    navigator.geolocation.getCurrentPosition(
+      this.successCallBack.bind(this),
+      this.errorCallBack,
+      options
+    );
+  }
+
+  searchAddressHandler(event: Event) {
+    event.preventDefault();
+    let address = this.inputElement.value;
+
+    axios
+      .get<GoogleGeoResponse>(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+          address
+        )}&key=${GOOGLE_API_KEY}`
+      )
+      .then((response) => {
+        if (response.data.status !== "OK") {
+          throw new Error("locationError");
+        }
+        const coordinates = response.data.results[0].geometry.location;
+        const { lat, lng } = coordinates;
+        const newCord = `${lat},${lng}`;
+
+        this.getWeathereData(newCord);
+        this.inputElement.value = "";
+      })
+      .catch((err: Error) => {
+        if (err.message === "locationError") {
+          this.showToolTip("Couldn't find that place");
+        } else {
+          this.showToolTip("Type city name");
+        }
+      });
+  }
+  showToolTip(tooltipTextContent: string) {
+    const tooltip = document.querySelector(".tooltip")! as HTMLDivElement;
+    const tooltipText = document.querySelector(
+      ".tooltiptext"
+    ) as HTMLSpanElement;
+    tooltipText.textContent = tooltipTextContent;
+    tooltip.style.visibility = "visible";
+    tooltipText.style.opacity = "1";
+    tooltipText.style.visibility = "visible";
+
+    setTimeout(() => {
+      tooltip.style.visibility = "hidden";
+      tooltipText.style.opacity = "0";
+    }, 2000);
+  }
+  getOtherWeatherInfo(weatherObj: any) {
+    const map = new Map<string, string>();
+    map.set("Chance of rain:", weatherObj.day.daily_chance_of_rain + "%");
+    map.set("Average humidity:", weatherObj.day.avghumidity + "%");
+    map.set("Maximum wind speed:", weatherObj.day.maxwind_kph + " km/h");
+    map.set("Average visibility:", weatherObj.day.avgvis_km + " km");
+    return map;
+  }
+  dataParse(weatherObj: any) {
+    return {
+      imgSrc: `${weatherObj.day.condition.icon}`,
+      date: weatherObj.date,
+      temp: weatherObj.day.avgtemp_c,
+    };
+  }
+  getWeathereData(cords: string) {
+    axios
+      .get("http://api.weatherapi.com/v1/forecast.json", {
+        params: {
+          key: WEATHER_API_KEY,
+          q: cords,
+          days: 3,
+        },
+      })
+      .then((response1) => {
+        this.setCityName(response1.data.location.name);
+        const weatherArray: object[] = response1.data.forecast.forecastday;
+
+        weatherArray.forEach((weatherObj) => {
+          const newObjc = this.dataParse(weatherObj);
+          const otherWeatherInfo = this.getOtherWeatherInfo(weatherObj);
+          new weatherModule(
+            newObjc.imgSrc,
+            newObjc.date,
+            newObjc.temp,
+            otherWeatherInfo
+          );
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  setCityName(cityName: string) {
+    this.cityNameElement.textContent = cityName;
+  }
+}
+
+class weatherModule {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+
+  constructor(
+    private iconLink: string,
+    private date: string,
+    private temperature: number,
+    private otherWeatherInfo: Map<string, string>
+  ) {
+    this.templateElement = document.getElementById(
+      "weatherModuleTemp"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.querySelector(
+      ".sectionWraper"
+    )! as HTMLDivElement;
+
+    this.renderModule();
+  }
+  configureModule(importedNode: DocumentFragment) {
+    const iconElement = importedNode.querySelector(
+      ".icon"
+    )! as HTMLImageElement;
+    const dateElement = importedNode.querySelector(
+      ".date"
+    )! as HTMLHeadingElement;
+    const temperatureElement = importedNode.querySelector(
+      ".temperature"
+    )! as HTMLHeadingElement;
+    const ulElement = importedNode.getElementById(
+      "otherInfo"
+    )! as HTMLUListElement;
+    iconElement.src = this.iconLink;
+    dateElement.textContent = this.date;
+    temperatureElement.textContent = `${this.temperature.toString()}°C`;
+    this.renderLiElements(ulElement);
+
+    return importedNode;
+  }
+  renderLiElements(ulElement: HTMLUListElement) {
+    for (const [key, val] of this.otherWeatherInfo) {
+      const newLiElement = document.createElement("li");
+      const newParagraphElement = document.createElement("p");
+      newParagraphElement.textContent = `${val}`;
+      newLiElement.textContent = `${key} `;
+      newLiElement.appendChild(newParagraphElement);
+      ulElement.appendChild(newLiElement);
+    }
+  }
+  renderModule() {
+    if (this.hostElement.children.length === 3) {
+      this.hostElement.innerHTML = "";
+    }
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    const configuretedNode = this.configureModule(importedNode);
+    this.hostElement.appendChild(configuretedNode);
+  }
+}
+
+class App {
+  static init() {
+    new Input();
+  }
+}
+
+App.init();
